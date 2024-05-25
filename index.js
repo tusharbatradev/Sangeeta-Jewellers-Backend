ac5441477d71e2c2b4efe763c892b4e6b8949db8
Aman <avman1998@gmail.com>
2024-05-25 12:12:54 +0530

    updated cors

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 const express = require("express");
-const cors=require("cors");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 //Parse the body
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+
+const corsOptions = {
+  origin: "https://new-invoice-backend.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 // Importing MongoDb connection function
-const {ConnectMongodb} = require('./connection')
+const { ConnectMongodb } = require("./connection");
 
 // Mongo URL
 const mongoUrl = process.env.MONGODB_URL;
 
 // Connection MongoDB
-ConnectMongodb(mongoUrl)
+ConnectMongodb(mongoUrl);
 
 // Importing Routers
-const productsRouter = require('./Routes/products');
-const userRouter=require("./Routes/userRouter")
-const invoiceRouter=require('./Routes/invoiceRouter')
+const productsRouter = require("./Routes/products");
+const userRouter = require("./Routes/userRouter");
+const invoiceRouter = require("./Routes/invoiceRouter");
 
 // Routes for different resources
-app.use("/product", productsRouter)
-app.use("/user",userRouter)
-app.use('/invoice',invoiceRouter)
+app.use("/product", productsRouter);
+app.use("/user", userRouter);
+app.use("/invoice", invoiceRouter);
 
 // Starting Server
-const port = process.env.PORT || 3000; 
-app.listen(port, () => console.log(`Server started on port ${port}`))
-
-
-
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
